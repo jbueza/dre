14a191630fcc418a550b5ab14b4bc06aec4a7344
Jaime Bueza <jbueza@gmail.com>
2012-10-13 03:30:21 -0700

    Moving findOne into MOdel

--- examples/simple-insertion.js
+++ examples/simple-insertion.js
@@ -3,15 +3,14 @@ var Client = require("../lib/Client");
 
 var dre = new Client(account);
 
-var Person = sdk.model("Person", dre.Schema({
+var Person = dre.model("People", dre.Schema({
   FirstName: "string",
   LastName: "string",
   FavoriteAnimal: "string"
 }));
 
-
 var jaime = new Person({
-  FirPartitionKey: "dcb83fa0-15c4-463f-99b7-2ce365878b20",
+  PartitionKey: "dcb83fa0-15c4-463f-99b7-2ce365878b20",
   RowKey: "58f92ada-2aa9-48f8-9429-4580e0f91b81",
   FirstName: "Jaime",
   LastName: "Bueza",
@@ -19,24 +18,6 @@ var jaime = new Person({
   DateJoined: new Date()
 })
 
-
 jaime.save(function(err, entity, response) {
-
-  // w00t
-});
-
-/*
-var person = client.define("People", {
-  PartitionKey: "dcb83fa0-15c4-463f-99b7-2ce365878b20",
-  RowKey: "58f92ada-2aa9-48f8-9429-4580e0f91b81",
-  FirstName: "Jaime",
-  LastName: "Bueza",
-  FavoriteAnimal: "Cat",
-  DateJoined: new Date()
-});*/
-
-
-person.save(function(err, entity, response) {
-
-  // w00t
+  console.log(entity);
 });
--- examples/simple-read.js
+++ examples/simple-read.js
@@ -1,18 +1,16 @@
 var account = require("../test/fixtures/secret.json");
 var Client = require("../lib/Client");
 
-var client = new Client(account);
+var dre = new Client(account);
 
-var person = client.define("People", {
-  PartitionKey: "dcb83fa0-15c4-463f-99b7-2ce365878b20",
-  RowKey: "58f92ada-2aa9-48f8-9429-4580e0f91b81",
-  FirstName: "Jaime",
-  LastName: "Bueza",
-  FavoriteAnimal: "Cat",
-  DateJoined: new Date()
-});
+var Person = dre.model("People", dre.Schema({
+  FirstName: "string",
+  LastName: "string",
+  FavoriteAnimal: "string"
+}));
 
-person.save(function(err, entity, response) {
-
-  // w00t
+Person.findOne({
+  FirstName: "Jaime"
+}, function(err, entity, response) {
+  console.log("Found you! ", entity);
 });
